acc02ed08125375577b55d9f7e23b8333611baad
Thanay Kumar Samala <thanaykumar235@gmail.com>
2026-06-28 14:39:35 +0530

    ✅ Two Sum | Easy | Java | Runtime: 0 ms | Memory: 42 MB | 2026-06-28

---
+++ leetcode/easy/0001-two-sum/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        int arr[] = new int[2];
+        for(int i=0;i<n;i++){
+            int complement= target-nums[i];
+            if(map.containsKey(complement)){
+                arr[0]=i;
+                arr[1]=map.get(complement);
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return arr;
+    }
+}
